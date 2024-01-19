3a800914a4fae4800a1f40f0692a322339f20340
lz520520 <610188221@qq.com>
2024-01-19 11:04:32 +0800

    update: websocket ipc

--- v2/pkg/assetserver/assetserver_dev.go
+++ v2/pkg/assetserver/assetserver_dev.go
@@ -4,8 +4,8 @@
 package assetserver
 
 import (
-	"net/http"
-	"strings"
+    "net/http"
+    "strings"
 )
 
 /*
@@ -14,18 +14,27 @@ Depending on the UserAgent it injects a websocket based IPC script into `index.h
 default desktop IPC is injected when the webview accesses the devserver.
 */
 func NewDevAssetServer(handler http.Handler, bindingsJSON string, servingFromDisk bool, logger Logger, runtime RuntimeAssets) (*AssetServer, error) {
-	result, err := NewAssetServerWithHandler(handler, bindingsJSON, servingFromDisk, logger, runtime)
-	if err != nil {
-		return nil, err
-	}
+    result, err := NewAssetServerWithHandler(handler, bindingsJSON, servingFromDisk, logger, runtime)
+    if err != nil {
+        return nil, err
+    }
 
-	result.appendSpinnerToBody = true
-	result.ipcJS = func(req *http.Request) []byte {
-		if strings.Contains(req.UserAgent(), WailsUserAgentValue) {
-			return runtime.DesktopIPC()
-		}
-		return runtime.WebsocketIPC()
-	}
+    result.appendSpinnerToBody = true
+    result.ipcJS = func(req *http.Request) []byte {
+        if strings.Contains(req.UserAgent(), WailsUserAgentValue) {
+            return runtime.DesktopIPC()
+        }
+        ipc := runtime.WebsocketIPC()
 
-	return result, nil
+        //if address, ok := os.LookupEnv("websocket_address"); ok {
+        //    ipc = bytes.ReplaceAll(ipc, []byte("window.location.host"), []byte(fmt.Sprintf(`"%s"`, address)))
+        //}
+        //if protocol, ok := os.LookupEnv("websocket_protocol"); ok {
+        //    ipc = bytes.ReplaceAll(ipc, []byte(`window.location.protocol.indexOf("https")`), []byte(fmt.Sprintf(`"%s".indexOf("wss")`, protocol)))
+        //}
+
+        return ipc
+    }
+
+    return result, nil
 }
